3529ee149ad09be3ac49ee54d9e496a5ca992cbf
Olivier Gamache <olivier@glo.quebec>
2022-10-22 04:12:01 -0400

    ci: use github action to replace process env

--- src/entry-server.tsx
+++ src/entry-server.tsx
@@ -1,6 +1,3 @@
 import { StartServer, createHandler, renderStream } from 'solid-start/entry-server'
-// import dotenv from 'dotenv'
-
-// dotenv.config()
 
 export default createHandler(renderStream(event => <StartServer event={event} />))
