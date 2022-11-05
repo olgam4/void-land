99d25ac233c16b4c68a80eb4d4ef289ff7b90f1e
Olivier Gamache <olivier@glo.quebec>
2022-11-04 22:19:49 -0400

    feat: no more waiting nor dl

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -9,15 +9,23 @@ type FeatureProps = {
   content: string
   image: string
 }
+//
+// TODO: Add a Download
+// <div class="p-12 h-[80vh] flex flex-col">
+//   <h2 id="download" class="text-4xl sm:text-8xl mb-12">Download.</h2>
+//   <div class="flex flex-wrap sm:flex-row-reverse flex-center justify-self-center">
+//     <div class="flex h-[200px] sm:ml-8 p-12 rounded-lg bg-white shadow-2xl">
+//       <a class="flex cursor-pointer hover:text-orange-200 transition-colors items-center" href="https://github.com/olgam4/nook" target="_blank" rel="noreferrer">
+//         <p class="text-center mr-2">Alpha avaliable <b><i>right now</i></b><br /> on GitHub</p>
+//         <div class="i-carbon-logo-github w-28 h-28" />
+//       </a>
+//     </div>
+//     <img class="w-[90%] mt-5 sm:mt-0 sm:w-[30%] sm:mb-0" srcset="/assets/download.svg" />
+//   </div>
+// </div>
 
 export default function() {
   let backgroundDom: any
-  let card: any
-
-  const [value, setValue] = createSignal('')
-  const [error, setError] = createSignal(false)
-  const [success, setSuccess] = createSignal(false)
-  const [loading, setLoading] = createSignal(false)
 
   const Feature = (props: FeatureProps) => {
     return (
@@ -100,50 +108,12 @@ export default function() {
     backgroundDom.style.backgroundImage = `url(${context.canvas.toDataURL('image/png')})`
   })
 
-  const onWait = () => {
-    card.classList.add('active')
-  }
-
-  const onCancel = () => {
-    card.classList.remove('active')
-  }
-
-  const validate = () => {
-    if (!value().match(/^([\w.%+-]+)@([\w-]+\.)+([\w]{2,})$/i)) {
-      setError(true)
-      return false
-    }
-    setError(false)
-    return true
-  }
-
-  const subscribe = (e: any) => {
-    e.preventDefault()
-
-    if (!validate() || loading()) return
-
-    setLoading(true)
-
-    fetch('/api/subscribe', {
-      method: 'POST',
-      body: JSON.stringify({ email: value() }),
-    }).then((res) => {
-      if (res.status === 200) {
-        setSuccess(true)
-
-      }
-    }).finally(() => {
-      setLoading(false)
-    })
-  }
-
   return (
     <div>
       <div class="flex flex-col h-[100vh] w-[100vw] bg-gradient-to-tr from-[#ffafbd] to-[#ffc3a0]">
         <nav class="hidden sm:block absolute mt-4 md:mt-0 top-16 right-8 z-50">
-          <ul class="grid grid-cols-4 text-xl md:gap-1 gap-2 text-white">
+          <ul class="grid grid-cols-3 text-xl md:gap-1 gap-2 text-white">
             <li class="w-[100px] text-right"><a class="cursor-pointer hover:font-black transition-all" href="#features">Features</a></li>
-            <li class="w-[100px] text-right"><a class="cursor-pointer hover:font-black transition-all" href="#download">Download</a></li>
             <li class="w-[100px] text-right"><a class="cursor-pointer hover:font-black transition-all" href="#pricing">Pricing</a></li>
             <li class="w-[160px] text-right"><a class="flex cursor-pointer items-center hover:font-black transition-all" target="_blank" rel="noreferrer" href="https://discord.gg/QXRMPDzXds">Chat with us <div class="i-carbon-arrow-right ml-2" /></a></li>
           </ul>
@@ -159,18 +129,6 @@ export default function() {
         <ul>
           <For each={features}>{(feature, index) => <Feature {...feature} left={index() % 2 !== 0} />}</For>
         </ul>
-      </div>
-      <div class="p-12 h-[80vh] flex flex-col">
-        <h2 id="download" class="text-4xl sm:text-8xl mb-12">Download.</h2>
-        <div class="flex flex-wrap sm:flex-row-reverse flex-center justify-self-center">
-          <div class="flex h-[200px] sm:ml-8 p-12 rounded-lg bg-white shadow-2xl">
-            <a class="flex cursor-pointer hover:text-orange-200 transition-colors items-center" href="https://github.com/olgam4/nook" target="_blank" rel="noreferrer">
-              <p class="text-center mr-2">Alpha avaliable <b><i>right now</i></b><br /> on GitHub</p>
-              <div class="i-carbon-logo-github w-28 h-28" />
-            </a>
-          </div>
-          <img class="w-[90%] mt-5 sm:mt-0 sm:w-[30%] sm:mb-0" srcset="/assets/download.svg" />
-        </div>
       </div>
       <div id="pricing" class="p-12 mb-20 bg-gradient-to-b from-[#ffc3a0] to-gray-50">
         <h2 class="text-4xl sm:text-8xl mb-12 text-white">Pricing.</h2>
@@ -204,74 +162,8 @@ export default function() {
         </div>
       </div>
       <div ref={backgroundDom} class="flex flex-col relative flex-center h-[100vh] overflow-hidden">
-        <div ref={card} class="flip hidden md:block relative">
-          <div class="flip-content py-48 text-center sm:w-[600px] rounded-xl shadow-2xl bg-gradient-to-b from-[#FFC0FF] to-[#FFB5DD]">
-            <p class="text-gray-700 sm:text-3xl top-0 sm:top-44 flip-front">Subscribe to the <button onClick={onWait} class="cursor-pointer underline underline-offset-4 text-gray-50 font-black ml-2 hover:text-[#A0003A] transition-colors">waiting list</button></p>
-            <div class="flip-back flex justify-center">
-              <Show when={!success()}
-                fallback={() => (
-                  <div class="text-3xl flex text-gray-600 font-black">
-                    <div class="i-carbon-checkmark-filled" />
-                    <p class="ml-3">Thank you for subscribing!</p>
-                  </div>
-                )}
-              >
-                <form class="absolute flex flex-col justify-around -top-48 p-10 w-full h-full">
-                  <div>
-                    <label class="block text-gray-700 text-left text-xl font-bold" for="email">
-                      Enter your email...
-                    </label>
-                    <input
-                      onInput={(e) => { setValue(e.currentTarget.value) }}
-                      id="email"
-                      class="w-full p-4 rounded-xl mb-4"
-                      type="email"
-                      placeholder="john.doe@email.com"
-                      onBlur={validate}
-                    />
-                    <Show when={error()}>
-                      <p class="text-red-500 font-bold">Please enter a valid email</p>
-                    </Show>
-                  </div>
-                  <input onClick={subscribe} type="submit" class="w-20 p-4 rounded-xl bg-gradient-to-l from-[#FFFADE] to-[#FFF7D0] text-black font-bold self-end cursor-pointer" value="JOIN" />
-                </form>
-                <button class="absolute cursor-pointer -top-44 left-2" onClick={onCancel}>
-                  <div class="i-carbon-arrow-left text-white text-3xl" />
-                </button>
-              </Show>
-            </div>
-          </div>
-        </div>
-        <div class="sm:hidden w-full py-10 px-6 bg-gradient-to-b from-[#FFC0FF] to-[#FFB5DD]">
-          <Show when={!success()}
-            fallback={() => (
-              <div class="text-3xl flex text-gray-600 font-black">
-                <div class="i-carbon-checkmark-filled" />
-                <p class="ml-3">Thank you for subscribing!</p>
-              </div>
-            )}
-          >
-            <form class="flex flex-col">
-              <h3 class="text-xl font-black mb-2">Subscribe to the waiting list</h3>
-              <div>
-                <label class="block text-gray-700 text-left text-xl font-bold" for="email">
-                  Enter your email...
-                </label>
-                <input
-                  onInput={(e) => { setValue(e.currentTarget.value) }}
-                  id="email"
-                  class="w-full p-4 rounded-xl mb-4"
-                  type="email"
-                  placeholder="john.doe@email.com"
-                  onBlur={validate}
-                />
-                <Show when={error()}>
-                  <p class="text-red-500 font-bold">Please enter a valid email</p>
-                </Show>
-              </div>
-              <input onClick={subscribe} type="submit" class="w-20 p-4 rounded-xl bg-gradient-to-l from-[#FFFADE] to-[#FFF7D0] text-black font-bold self-end cursor-pointer" value="JOIN" />
-            </form>
-          </Show>
+        <div class="p-40 text-3xl text-center rounded-xl shadow-2xl bg-gradient-to-b from-[#FFC0FF] to-[#FFB5DD]">
+          <p class="text-gray-700">Join in the<a href="/account" class="cursor-pointer underline underline-offset-4 text-gray-50 font-black ml-2 hover:text-[#A0003A] transition-colors">fun</a></p>
         </div>
         <div class="absolute bottom-10 left-0 right-0 flex justify-center">
           <ul class="flex space-x-5">
